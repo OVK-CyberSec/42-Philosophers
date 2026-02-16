28753e5b6ad64a8301edc6bfe6061b4ae759018f
Amine Hifdi <101675260+OVK-CyberSec@users.noreply.github.com>
2026-02-16 17:22:04 +0100

    Update init.c

--- init.c
+++ init.c
@@ -1,30 +1,5 @@
 #include "philo.h"
 
-static int	validate_args(t_data *data, int ac)
-{
-	if (data->num_philos < 1 || data->num_philos > 200)
-		return (0);
-	if (data->time_to_die < 0 || data->time_to_eat < 0)
-		return (0);
-	if (data->time_to_sleep < 0)
-		return (0);
-	if (ac == 6 && data->must_eat_count < 0)
-		return (0);
-	return (1);
-}
-
-static void	parse_args(t_data *data, int ac, char **av)
-{
-	data->num_philos = ft_atoi(av[1]);
-	data->time_to_die = ft_atoi(av[2]);
-	data->time_to_eat = ft_atoi(av[3]);
-	data->time_to_sleep = ft_atoi(av[4]);
-	if (ac == 6)
-		data->must_eat_count = ft_atoi(av[5]);
-	else
-		data->must_eat_count = -1;
-}
-
 static int	init_mutexes(t_data *data)
 {
 	if (pthread_mutex_init(&data->write_lock, NULL) != 0)
